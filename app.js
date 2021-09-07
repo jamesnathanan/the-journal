c6f396e659c629360dd6bab608dfafb3661f34b7
jamesnathanan <artofgems@live.com>
2021-09-08 03:05:23 +0700

    create object from req.body

--- app.js
+++ app.js
@@ -22,6 +22,25 @@ app.get("/", (req, res) => {
   res.render("home", { startingContent: homeStartingContent });
 });
 
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+app.post("/compose", (req, res) => {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+});
+
 app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
