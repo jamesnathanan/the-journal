e04b8e60e89432d23c1aa6270d75b71495da182a
jamesnathanan <artofgems@live.com>
2021-09-09 06:34:21 +0700

    learn about express routing and params

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const aboutContent =
 const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+const posts = [];
+
 const app = express();
 
 app.set("view engine", "ejs");
@@ -19,7 +21,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { startingContent: homeStartingContent });
+  res.render("home", { startingContent: homeStartingContent, posts });
+  // console.log(posts);
 });
 
 app.get("/about", (req, res) => {
@@ -34,11 +37,23 @@ app.get("/compose", (req, res) => {
   res.render("compose");
 });
 
+app.get("/posts/:postName", (req, res) => {
+  const requestedTitle = req.params.postName;
+  posts.forEach((post) => {
+    const storedTitle = post.title;
+    if (storedTitle === requestedTitle) {
+      console.log("Math Found");
+    }
+  });
+});
+
 app.post("/compose", (req, res) => {
   const post = {
     title: req.body.postTitle,
     content: req.body.postBody,
   };
+  posts.push(post);
+  res.redirect("/");
 });
 
 app.listen(3000, function () {
